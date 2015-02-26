c5c9e7be053472fd585254729b69c4398a7c8f4f
Juan López Rubio <j.lopez.r@gmail.com>
2015-02-26 20:20:20 +0100

    Initial commit

---
+++ EjemploMongoDB/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace EjemploMongoDB
+{
+    class Program
+    {
+        public class Entity
+        {
+            public ObjectId Id { get; set; }
+
+            public string Address { get; set; }
+
+            public double Value { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            var connectionString = "mongodb://10.211.55.1";
+            var client = new MongoClient(connectionString);
+            var server = client.GetServer();
+            var database = server.GetDatabase("DatosAereos");
+
+            Entity e = new Entity();
+            e.Address = "altimeter";
+            e.Value = 100;
+
+            var collection = database.GetCollection<Entity>("log");
+            collection.Insert(e);
+            var id = e.Id;
+        }
+    }
+}
