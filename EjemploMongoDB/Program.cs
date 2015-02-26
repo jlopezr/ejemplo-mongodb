ccf8428ddd7cb4d3207d9d6789613d6d2205722f
Juan López Rubio <j.lopez.r@gmail.com>
2015-02-26 20:21:57 +0100

    Added comments

--- EjemploMongoDB/Program.cs
+++ EjemploMongoDB/Program.cs
@@ -17,15 +17,18 @@ namespace EjemploMongoDB
 
         static void Main(string[] args)
         {
+            // Esto se conecta al servidor
             var connectionString = "mongodb://10.211.55.1";
             var client = new MongoClient(connectionString);
             var server = client.GetServer();
             var database = server.GetDatabase("DatosAereos");
 
+            // Esto construye una nueva instancia
             Entity e = new Entity();
             e.Address = "altimeter";
             e.Value = 100;
 
+            // Esto lo sube al mongodb
             var collection = database.GetCollection<Entity>("log");
             collection.Insert(e);
             var id = e.Id;
