ea462257e4d7dbc5d907f035cb46fa38944a5742
marc1993 <marcmorales2204@hotmail.com>
2015-03-16 11:12:23 +0100

    Update and rename  Main to Program.cs

--- Primer-commit/Program.cs
+++ Primer-commit/Program.cs
@@ -1,10 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MongoDB.Bson;
-using MongoDB.Driver;
 using Classes;
 
 namespace MongoDBExample
@@ -19,19 +15,25 @@ namespace MongoDBExample
             Console.WriteLine("Type 3 to research by Time.");
             Console.WriteLine("Type 4 to research by Name.");
             Console.WriteLine("Type 5 to remove all data stored");
-
+            
             int choice = Convert.ToInt32(Console.ReadLine());
-
+            
             if (choice == 1)
             {
                 Console.WriteLine("Type an address.");
                 string address = Console.ReadLine();
                 Console.WriteLine("Type a value.");
                 double value = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter the latitude.");
+                double lat = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter the longitude.");
+                double lon = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter the altitude.");
+                double alt = Convert.ToDouble(Console.ReadLine());
 
                 // This creates the database and executes the AddLog function
                 DataBase db = new DataBase();
-                db.AddLog(address, value);
+                db.AddLog(address, value, lat, lon, alt);
             }
             else if (choice == 2)
             {
@@ -40,7 +42,7 @@ namespace MongoDBExample
                 int confirmation = 1;
 
                 Console.WriteLine("Add as much paired addresses and values as you want.");
-
+                
                 // This keeps asking the user for address and value until he wants to stop
                 while (confirmation != 0)
                 {
@@ -51,6 +53,15 @@ namespace MongoDBExample
                     Console.WriteLine("Type a value.");
                     double value = Convert.ToDouble(Console.ReadLine());
                     e.Value = value;
+                    Console.WriteLine("Enter the latitude.");
+                    double lat = Convert.ToDouble(Console.ReadLine());
+                    e.Latitude = lat;
+                    Console.WriteLine("Enter the longitude.");
+                    double lon = Convert.ToDouble(Console.ReadLine());
+                    e.Longitude = lon;
+                    Console.WriteLine("Enter the altitude.");
+                    double alt = Convert.ToDouble(Console.ReadLine());
+                    e.Altitude = alt;
 
                     LogList.Add(e);
 
@@ -64,10 +75,9 @@ namespace MongoDBExample
                 Console.WriteLine("The logs have been added to the DataBase");
 
             }
+
             else if (choice == 3)
             {
-                List<Entity> LogList = new List<Entity>();
-
                 Console.WriteLine("Type the lower value limit");
                 double lowlimit = Convert.ToInt32(Console.ReadLine());
 
@@ -76,7 +86,7 @@ namespace MongoDBExample
 
                 // This creates the database and executes the SearchByTime function
                 DataBase db = new DataBase();
-                db.SearchByTime(lowlimit, highlimit);
+                List<Entity> LogList = db.SearchByTime(lowlimit, highlimit);
 
                 int count = LogList.Count;
 
@@ -84,23 +94,28 @@ namespace MongoDBExample
                 for (int i = 0; i < count; i++)
                 {
                     Entity e = new Entity();
-                    e.Value = LogList.ElementAt(i).Value;
                     e.Address = LogList.ElementAt(i).Address;
-                    string print1 = Convert.ToString(e.Value);
-                    string print2 = Convert.ToString(e.Address);
-                    Console.WriteLine("Address: {0}, Value: {1}", print1, print2);
+                    e.Value = LogList.ElementAt(i).Value;
+                    e.Latitude = LogList.ElementAt(i).Latitude;
+                    e.Longitude = LogList.ElementAt(i).Longitude;
+                    e.Altitude = LogList.ElementAt(i).Altitude;
+                    string print1 = Convert.ToString(e.Address);
+                    string print2 = Convert.ToString(e.Value);
+                    string print3 = Convert.ToString(e.Latitude);
+                    string print4 = Convert.ToString(e.Longitude);
+                    string print5 = Convert.ToString(e.Altitude);
+                    Console.WriteLine("Address: {0}, Value: {1}, Latitude: {2}, Longitude: {3}, Altitude: {4}", print1, print2, print3, print4, print5);
                 }
             }
+
             else if (choice == 4)
             {
-                List<Entity> LogList = new List<Entity>();
-
                 Console.WriteLine("Type the name you wish to search for");
                 string name = Convert.ToString(Console.ReadLine());
 
                 // This creates the database and executes the SearchByAddress function
                 DataBase db = new DataBase();
-                db.SearchByAddress(name);
+                List<Entity> LogList = db.SearchByAddress(name);
 
                 int count = LogList.Count;
 
@@ -108,13 +123,20 @@ namespace MongoDBExample
                 for (int i = 0; i < count; i++)
                 {
                     Entity e = new Entity();
-                    e.Value = LogList.ElementAt(i).Value;
                     e.Address = LogList.ElementAt(i).Address;
-                    string print1 = Convert.ToString(e.Value);
-                    string print2 = Convert.ToString(e.Address);
-                    Console.WriteLine("Address: {0}, Value: {1}", print1, print2);
+                    e.Value = LogList.ElementAt(i).Value;
+                    e.Latitude = LogList.ElementAt(i).Latitude;
+                    e.Longitude = LogList.ElementAt(i).Longitude;
+                    e.Altitude = LogList.ElementAt(i).Altitude;
+                    string print1 = Convert.ToString(e.Address);
+                    string print2 = Convert.ToString(e.Value);
+                    string print3 = Convert.ToString(e.Latitude);
+                    string print4 = Convert.ToString(e.Longitude);
+                    string print5 = Convert.ToString(e.Altitude);
+                    Console.WriteLine("Address: {0}, Value: {1}, Latitude: {2}, Longitude: {3}, Altitude: {4}", print1, print2, print3, print4, print5);
                 }
             }
+
             else if (choice == 5)
             {
                 // This creates the database and executes the RemoveAll function
