6ffc9b3a67283dbb846322fdbe25dda167109e7f
marc1993 <marcmorales2204@hotmail.com>
2015-03-16 11:13:32 +0100

    Create Entity.cs

---
+++ Primer-commit/Entity.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+
+
+namespace Classes
+{
+        public class Entity
+        {
+            public ObjectId Id { get; set; }
+
+            public string Address { get; set; }
+
+            public double Value { get; set; }
+
+            public double Latitude { get; set; }
+
+            public double Longitude { get; set; }
+
+            public double Altitude { get; set; }
+        }
+
+}
